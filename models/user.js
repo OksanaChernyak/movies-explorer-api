0a4fbebda886340463074c69e6ceda49c0780491
Oksana <oksanaadams@yandex.ru>
2022-09-11 16:10:46 +0300

    added file structure

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const isEmail = require('validator/lib/isEmail');
+const UnauthorizedError = require('../utils/UnauthorizedError');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minlength: 2,
+        maxlength: 30,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: isEmail,
+        },
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+});
+/*
+userSchema.statics.findUserByCredentials = function (email, password) {
+    return this.findOne({ email }).select('+password')
+        .then((user) => {
+            if (!user) {
+                // 401
+                throw new UnauthorizedError('Неправильные почта или пароль');
+            }
+            return bcrypt.compare(password, user.password)
+                .then((matched) => {
+                    if (!matched) {
+                        throw new UnauthorizedError('Неправильные почта или пароль');
+                    }
+                    return user;
+                });
+        });
+};
+*/
+module.exports = mongoose.model('user', userSchema);
