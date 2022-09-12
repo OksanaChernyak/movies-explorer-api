4821cc3efb084bcecc817e1c3ce993d7f06cabd8
OksanaChe <oksanaadams@yandex.ru>
2022-09-12 18:03:24 +0300

    fix: joi1

--- models/movie.js
+++ models/movie.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({
     country: {
         type: String,
-        required: true,
+        required: [true, 'Заполните поле country'],
     },
     director: {
         type: String,
