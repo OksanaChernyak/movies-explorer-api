63774554189feb9bd19c1abfb9d110828216d60a
OksanaChe <oksanaadams@yandex.ru>
2022-09-12 18:50:12 +0300

    fix: joi2

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { BadRequestError } = require(../utils/BadRequestError);
 
 const {
     getMovies, createMovie, deleteMovieById,
@@ -14,18 +13,18 @@ router.delete('/_id', celebrate({
 }), deleteMovieById);
 router.post('/', celebrate({
     body: Joi.object().keys({
-        country: Joi.string().required().error(new BadRequestError('country')),
-        director: Joi.string().required().error(new BadRequestError('director')),
-        duration: Joi.number().required().error(new BadRequestError('duration')),
-        year: Joi.string().required().error(new BadRequestError('year')),
-        description: Joi.string().required().error(new BadRequestError('description')),
-        image: Joi.string().required().regex(/^https?:\/\/(www.)?([\da-z-]+\.)+\/?\S*/im).error(new BadRequestError('img')),
-        trailerLink: Joi.string().required().regex(/^https?:\/\/(www.)?([\da-z-]+\.)+\/?\S*/im).error(new BadRequestError('trailer')),
-        thumbnail: Joi.string().required().regex(/^https?:\/\/(www.)?([\da-z-]+\.)+\/?\S*/im).error(new BadRequestError('thumb')),
-        owner: Joi.string().alphanum().length(24).hex().error(new BadRequestError('owner')),
-        movieId: Joi.number().required().error(new BadRequestError('movieId')),
-        nameRU: Joi.string().required().regex(/^[а-яА-ЯёЁ\d\s]+$/mi).error(new BadRequestError('nameRU')),
-        nameEN: Joi.string().required().regex(/^[\w\s]+$/mi).error(new BadRequestError('nameEN')),
+        country: Joi.string().required(),
+        director: Joi.string().required(),
+        duration: Joi.number().required(),
+        year: Joi.string().required(),
+        description: Joi.string().required(),
+        image: Joi.string().required().regex(/^https?:\/\/(www.)?([\da-z-]+\.)+\/?\S*/im),
+        trailerLink: Joi.string().required().regex(/^https?:\/\/(www.)?([\da-z-]+\.)+\/?\S*/im),
+        thumbnail: Joi.string().required().regex(/^https?:\/\/(www.)?([\da-z-]+\.)+\/?\S*/im),
+        owner: Joi.string().alphanum().length(24).hex(),
+        movieId: Joi.number().required(),
+        nameRU: Joi.string().required().regex(/^[а-яА-ЯёЁ\d\s]+$/mi),
+        nameEN: Joi.string().required().regex(/^[\w\s]+$/mi),
     }),
 }), createMovie);
 module.exports = router;
