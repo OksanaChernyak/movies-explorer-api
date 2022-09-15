91263761e84f5ef97f1304a6c227f0bfb7200729
OksanaChe <oksanaadams@yandex.ru>
2022-09-15 12:32:05 +0300

    fix: routes and helmet

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
+require('dotenv').config();
 const cors = require('cors');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { errors, celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
 const express = require('express');
-const userRoute = require('./routes/users');
-const movieRoute = require('./routes/movies');
-const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
+const indexRoutes = require('./routes/index_routes');
 const errorsHandler = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const NotFoundError = require('./utils/NotFoundError');
 const { MDB_DEV } = require('./utils/constants');
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
 const { NODE_ENV, MDB_URL } = process.env;
-
-require('dotenv').config();
 
 mongoose.connect(NODE_ENV === 'production' ? MDB_URL : MDB_DEV, {
   useNewUrlParser: true,
@@ -27,28 +23,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
+app.use(helmet());
 app.use(requestLogger);
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
+app.use('/', indexRoutes);
 
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-  }),
-}), createUser);
-
-app.use('/users', auth, userRoute);
-app.use('/movies', auth, movieRoute);
-app.use('*', auth, () => {
-  throw new NotFoundError('Страница  по этому адресу не найдена');
-});
 app.use(errorLogger);
 app.use(errors());
 app.use(errorsHandler);
