46b88b1065412677a15c574d7e22a0971e82279f
OksanaChe <oksanaadams@yandex.ru>
2022-10-09 20:24:49 +0300

    fix: nameRU

--- routes/index.js
+++ routes/index.js
@@ -15,9 +15,9 @@ router.post('/signin', celebrate({
 
 router.post('/signup', celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30).required(),
   }),
 }), createUser);
 
